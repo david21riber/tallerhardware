96cf59a43c54b499da2a4419df0d807bda630aa4
david21riber <david21_riberalt@hotmail.es>
2016-12-08 01:36:10 -0400

    reports function

--- arduino.controller.js
+++ arduino.controller.js
@@ -6,7 +6,22 @@ var app = angular.module('tallerApp', ['ngStorage']);
 app.controller('stylerMainCrtl', function($scope, $localStorage) {
 $scope.hi = 'hello word';
 
-  $localStorage['param'] = {"a":1,"b":2};
+  //$localStorage['param'] = {"a":1,"b":2};
+
+  $scope.setButton = function(led, state){
+    var objSave = {"label":led,"state":state, "time": new Date()};
+    console.log('ty', objSave);
+    var localStorange =  $localStorage['report'];
+    if(localStorange && localStorange.length>0){
+    localStorange.push(objSave);
+    $localStorage['report'] =  localStorange;
+    }else{
+      $localStorage['report'] = [objSave];
+
+    }
+
+
+  }
 
 });
 
--- index.php
+++ index.php
@@ -53,25 +53,27 @@ $usb_comPort = "COM4";
 ?>
 </head>
 
-<body>
+<body ng-app="tallerApp" ng-controller="stylerMainCrtl" >
+<h1>{{hi}}</h1>
+
 
 <CENTER><h1>LEDS</h1></CENTER>
 
 <CENTER>	<form  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<input type='submit' name='submitOn' value='LED 1 ENCENDER'>
-		<input type='submit' name='submitOff' value='LED 1 APAGAR'>
+		<input type='submit' ng-click="setButton('Led 1', 'On')" name='submitOn' value='LED 1 ENCENDER'>
+		<input type='submit' ng-click="setButton('Led 1', 'Off')" name='submitOff' value='LED 1 APAGAR'>
 		<br><br>
-		<input type='submit'  name='submitOn1'  value='LED 2 ENCENDER'>
-		<input type='submit'  name='submitOff1' value='LED 2 APAGAR'>
+		<input type='submit' ng-click="setButton('Led 2', 'On')" name='submitOn1'  value='LED 2 ENCENDER'>
+		<input type='submit' ng-click="setButton('Led 2', 'Off')" name='submitOff1' value='LED 2 APAGAR'>
 		<br><br>
-		<input type='submit'  name='submitOn2'  value='LED 3 ENCENDER'>
-		<input type='submit'  name='submitOff2'  value='LED 3 APAGAR'>
+		<input type='submit' ng-click="setButton('Led 3', 'On')" name='submitOn2'  value='LED 3 ENCENDER'>
+		<input type='submit' ng-click="setButton('Led 3', 'Off')" name='submitOff2'  value='LED 3 APAGAR'>
 		<br><br>
-		<input type='submit'  name='submitOn3'  value='LED 4 ENCENDER'>
-		<input type='submit'  name='submitOff3'  value='LED 4 APAGAR'>
+		<input type='submit' ng-click="setButton('Led 4', 'On')" name='submitOn3'  value='LED 4 ENCENDER'>
+		<input type='submit' ng-click="setButton('Led 4', 'Off')" name='submitOff3'  value='LED 4 APAGAR'>
 		<br><br>
-		<input type='submit'  name='submitOn4'  value='LED 5 ENCENDER'>
-		<input type='submit'  name='submitOff4'  value='LED 5 APAGAR'>
+		<input type='submit' ng-click="setButton('Led 5', 'On')" name='submitOn4'  value='LED 5 ENCENDER'>
+		<input type='submit' ng-click="setButton('Led 5', 'Off')" name='submitOff4'  value='LED 5 APAGAR'>
 
 
 		<br><br>
@@ -79,7 +81,16 @@ $usb_comPort = "COM4";
 		<input type='submit' name='alloff' value='APAGAR TODO'>
 		
 	</form></CENTER>
-
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="bower_components/angular/angular.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
+<script src="bower_components/ngstorage/ngStorage.min.js"></script>
+<script src="bower_components/angular-animate/angular-animate.js"></script>
+<script src="bower_components/angular-cookies/angular-cookies.js"></script>
+<script src="bower_components/angular-resource/angular-resource.js"></script>
+<script src="bower_components/angular-route/angular-route.js"></script>
+<script src="bower_components/angular-sanitize/angular-sanitize.js"></script>
+<script src="bower_components/angular-touch/angular-touch.js"></script>
 <script src="arduino.controller.js"></script>
 </body>
 </html>
